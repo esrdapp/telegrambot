f4e2fcad8e81e06a1a612760f5f5ddb864ddac68
ESR <82104698+esrdapp@users.noreply.github.com>
2023-01-08 17:58:43 +0000

    Update bot.js

--- functions/bot.js
+++ functions/bot.js
@@ -4,11 +4,6 @@ const { BinanceChain } = require('@binance-chain/javascript-sdk');
 //const BOT_TOKEN = process.env.BOT_TOKEN;
 const BOT_TOKEN = "5963723556:AAGi_6gnn_Wk9s84hVZFTfQpEV9N52FcHqQ";
 
-Use this token to access the HTTP API:
-5963723556:AAGi_6gnn_Wk9s84hVZFTfQpEV9N52FcHqQ
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 // Connect to a Binance Chain node
 const binanceChain = new BinanceChain('https://bsc-dataseed.binance.org/');
 
