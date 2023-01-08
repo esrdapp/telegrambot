714f1d1aaa903be28484f7651860ad346c3ab027
ESR <82104698+esrdapp@users.noreply.github.com>
2023-01-08 20:55:53 +0000

    Update bot.js

--- functions/bot.js
+++ functions/bot.js
@@ -1,29 +1,27 @@
 const TelegramBot = require('node-telegram-bot-api');
-// const { BinanceChain } = require('@binance-chain/javascript-sdk');
-
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const chatId = "-1001800847763";
+//const { BinanceChain } = require('@binance-chain/javascript-sdk');
+// Load the ABI for your smart contract
+// const contractABI = require('./abi.json');
 
 // Connect to a Binance Chain node
 // const binanceChain = new BinanceChain('https://bsc-dataseed1.binance.org:443');
+
+// Create an instance of the contract
+// const contract = new binanceChain.contract(contractABI, contractAddress);
 
 const Web3 = require('web3');
 
 // Create a new connection to the Binance Smart Chain
 const w3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed1.binance.org:443'));
 
-// Load the ABI for your smart contract
-const contractABI = require('./abi.json');
-
-// Set the contract address
 const contractAddress = '0xd2d7289DB68395593D65101753Fec9450ddFB699';
 
-// Create an instance of the contract
-// const contract = new binanceChain.contract(contractABI, contractAddress);
+const chatId = "-1001800847763";
 
-const bot = new TelegramBot(BOT_TOKEN, {polling: true});
+// Replace YOUR_BOT_TOKEN with your bot's token
+const bot = new TelegramBot('5963723556:AAGi_6gnn_Wk9s84hVZFTfQpEV9N52FcHqQ', {polling: true});
 
-bot.onText(/\/balance/, (message) => {
+bot.onText(/\/balance/, message => {
   // Get the chat ID and message ID of the message
   const chatId = message.chat.id;
   const messageId = message.message_id;
@@ -32,17 +30,14 @@ bot.onText(/\/balance/, (message) => {
   w3.eth.getBalance(contractAddress, (error, result) => {
     if (error) {
       console.error(error);
-      bot.sendMessage(
-        chatId,
-        "Sorry, there was an error getting the balance of the contract."
-      );
+      bot.sendMessage(chatId, 'Sorry, there was an error getting the balance of the contract.');
     } else {
       // Send a message with the contract balance
-      bot.sendMessage(chatId, `The balance of the contract is ${result} wei.`, {
-        reply_to_message_id: messageId,
-      });
+      bot.sendMessage(chatId, `The balance of the contract is ${result} wei.`, {reply_to_message_id: messageId});
     }
-  });
+  }
+
+ );
 });
 
 
